7bcbaec7fae1a3503dd060276d6b8666c383bff9
Андрей Пулин <kaoru-san@yandex.ru>
2017-05-15 23:35:31 +0300

    Карусель начал
    Подключил скрипт, стили карусели, всё, кажется, заработало.

---
+++ html/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Sada-Mac on 16/05/17.
+ */
+
+$('.variable-width').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    variableWidth: true
+});
