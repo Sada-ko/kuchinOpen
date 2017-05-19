3bee42418e56b43a8d4b01276eeac06fd0875a2f
ANDREY_DESIGNER\Andrey <kaoru-san@yandex.ru>
2017-05-19 19:15:27 +0300

    Включил таблицу
    Добавил мячики в шапку
    Поправил главное меню

--- html/js/app.js
+++ html/js/app.js
@@ -10,15 +10,10 @@ $(document).ready(function () {
 //Owl Carousel
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
-        margin:10,
+        //margin:10,
         nav: true,
-        //navElement: "p",
-        //loop:true,
-        //navText: ["", ""],
         navText: ["<a class='w-100 h-100 d-block border-0'></a>","<a class='w-100 h-100 d-block border-0'></a>"],
-        autoWidth:true,
-        items:4
+        autoWidth:true
+        //items:4
     });
-    //$( ".owl-prev").html('<a class="owl-prev border-0"></a>:);
-    //$( ".owl-next").html('<a class="owl-next border-0"></a>');
 });
