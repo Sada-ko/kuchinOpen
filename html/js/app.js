444f399e6c8aa1c1c3f2f79682b122ac658a346f
ANDREY_DESIGNER\Andrey <kaoru-san@yandex.ru>
2017-05-19 17:59:06 +0300

    Шрифт с мячиками
    Сделал шрифт с мячиками.
    Прикрепил его к поиску игроков.
    И к лучшим игрокам.

--- html/js/app.js
+++ html/js/app.js
@@ -2,19 +2,6 @@
  * Created by Sada-Mac on 16/05/17.
  */
 
-//Slick Carousel
-//$('.slick-carousel').slick({
-//    //dots: true,
-//    infinite: false,
-//    speed: 300,
-//    slidesToShow: 1,
-//    variableWidth: true,
-//    swipeToSlide: true
-    //rtl: true
-    //adaptiveHeight: true,
-    //slidesToScroll: 4
-//});
-
 //BG images
 $(document).ready(function () {
     $('.fill-box').fillBox();
