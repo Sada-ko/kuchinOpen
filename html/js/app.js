5303621ada0bc29d260bbdc57fee41cfcbed800e
ANDREY_DESIGNER\Andrey <kaoru-san@yandex.ru>
2017-05-24 18:39:05 +0300

    Поправил общие типографические стили

--- html/js/app.js
+++ html/js/app.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         //margin:10,
         nav: true,
-        navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right' aria-hidden='true'></i>"],
+        navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right ml-3' aria-hidden='true'></i>"],
         autoWidth:true
         //items:4
     });
