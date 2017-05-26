aea31e4b4da6df6c66af075e450e0577a665c84a
ANDREY_DESIGNER\Andrey <kaoru-san@yandex.ru>
2017-05-26 17:15:49 +0300

    Страница Событие
    Начал делать страницу события
    Начал делать таблицу результатов

--- html/js/app.js
+++ html/js/app.js
@@ -5,15 +5,18 @@
 //BG images
 $(document).ready(function () {
     $('.fill-box').fillBox();
-});
 
-//Owl Carousel
-$(document).ready(function(){
+    //Owl Carousel
+    $(".event_short_gallery.owl-carousel").owlCarousel({
+        nav: true,
+        loop:true,
+        navText: ["<i class='fa fa-angle-left fa-2x' aria-hidden='true'></i>","<i class='fa fa-angle-right fa-2x ml-3' aria-hidden='true'></i>"],
+        autoWidth:true
+    });
+
     $(".owl-carousel").owlCarousel({
-        //margin:10,
         nav: true,
         navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right ml-3' aria-hidden='true'></i>"],
         autoWidth:true
-        //items:4
     });
 });
